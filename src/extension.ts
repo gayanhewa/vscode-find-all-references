666c7aef8493c1c894c310a5ea34c04878c61680
Gayan Hewa <gayanhewa@gmail.com>
2017-07-29 10:42:55 +1000

    Fixing the selected line number issue and cleaning up

--- src/extension.ts
+++ src/extension.ts
@@ -13,32 +13,30 @@ class PHPReferenceProvider implements vscode.ReferenceProvider {
         Thenable<vscode.Location[]> {
 
         return new Promise<vscode.Location[]>((resolve, reject) => {
-
             const searchTerm = document.getText(document.getWordRangeAtPosition(position));
-            let rg = child_process.spawn('rg', ['--column', searchTerm, vscode.workspace.rootPath]);
 
-            rg.stdout.on('data', (data) => {
-                let list = [];
-                let lines = data.toString().split('\n');
-                lines.forEach((item) => {
-                    let arr = item.split(":");
+            let rg = child_process.spawnSync('rg', ['--column', searchTerm, vscode.workspace.rootPath]);
+            let lines = rg.stdout.toString().split('\n');
 
-                    if (arr.length>2) {
-                        let range = new vscode.Range(new vscode.Position(parseInt(arr[1]), parseInt(arr[2])), new vscode.Position(parseInt(arr[1]), parseInt(arr[2])));
-                        list.push(new vscode.Location(vscode.Uri.file(arr[0]), range));
-                    }
-                });
+            let list = [];
 
-                return resolve(list);
+            lines.forEach((item) => {
+                let arr = item.split(":");
+
+                if (arr.length>2) {
+                    // Position starts from zero refer to vscode.d.ts
+                    let line_no = parseInt(arr[1])-1;
+                    let col_no = parseInt(arr[2])-1;
+
+                    let start_pos = new vscode.Position(line_no, col_no);
+                    let end_pos = new vscode.Position(line_no, col_no+searchTerm.length);
+                    let loc = new vscode.Location(vscode.Uri.file(arr[0]), new vscode.Range(start_pos, end_pos));
+                    console.log(loc);
+                    list.push(loc);
+                }
             });
 
-            rg.stderr.on('data', (data) => {
-                console.log(`stderr: ${data}`);
-            });
-
-            rg.on('close', (code) => {
-                console.log(`child process exited with code ${code}`);
-            });
+            return resolve(list);
         });
     }
 }
@@ -46,11 +44,9 @@ class PHPReferenceProvider implements vscode.ReferenceProvider {
 
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log('stated');
     context.subscriptions.push(
         vscode.languages.registerReferenceProvider(
             PHP_MODE, new PHPReferenceProvider()));
-
 }
 
 // this method is called when your extension is deactivated
