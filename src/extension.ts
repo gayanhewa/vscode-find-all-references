65cb5f598c6c0ec84d1eadc9faecdf7ba8b59725
Gayan Hewa <gayanhewa@gmail.com>
2017-07-29 10:42:54 +1000

    Adding ripgrep rust plugin for reference search

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
 'use strict';
 
 import * as vscode from 'vscode';
+import * as grep from 'git-grep';
+import * as child_process from 'child_process';
 
 const PHP_MODE = 'php';
 
@@ -10,14 +12,33 @@ class PHPReferenceProvider implements vscode.ReferenceProvider {
         options: { includeDeclaration: boolean }, token: vscode.CancellationToken):
         Thenable<vscode.Location[]> {
 
-        return new Promise((resolve, reject) => {
+        return new Promise<vscode.Location[]>((resolve, reject) => {
 
-            let uri = vscode.Uri.parse("/Users/gayan.hewa/Workspace/phphelper/index.php");
-            let pos =  new vscode.Position(8,22);
-            console.log(pos);
+            const searchTerm = document.getText(document.getWordRangeAtPosition(position));
+            let rg = child_process.spawn('rg', ['--column', searchTerm, '/Users/gayan.hewa/Workspace/phphelper']);
 
-            let arr = [new vscode.Location(uri, pos)];
-            return arr;
+            rg.stdout.on('data', (data) => {
+                let list = [];
+                let lines = data.toString().split('\n');
+                lines.forEach((item) => {
+                    let arr = item.split(":");
+
+                    if (arr.length>2) {
+                        let range = new vscode.Range(new vscode.Position(parseInt(arr[1]), parseInt(arr[2])), new vscode.Position(parseInt(arr[1]), parseInt(arr[2])));
+                        list.push(new vscode.Location(vscode.Uri.file(arr[0]), range));
+                    }
+                });
+
+                return resolve(list);
+            });
+
+            rg.stderr.on('data', (data) => {
+                console.log(`stderr: ${data}`);
+            });
+
+            rg.on('close', (code) => {
+                console.log(`child process exited with code ${code}`);
+            });
         });
     }
 }
