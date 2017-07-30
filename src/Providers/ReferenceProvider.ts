02444a5446ead50b4658162e1f24965bfbceee83
Gayan Hewa <gayanhewa@gmail.com>
2017-07-31 02:08:51 +1000

    Fixing the issue with the extra space

--- src/Providers/ReferenceProvider.ts
+++ src/Providers/ReferenceProvider.ts
@@ -11,16 +11,40 @@ export class ReferenceProvider implements vscode.ReferenceProvider {
         options: { includeDeclaration: boolean }, token: vscode.CancellationToken):
         Thenable<vscode.Location[]> {
 
+        return this.getEngineOptions().then((engineOptions) => {
+            return this.processSearch(document, position, engineOptions);
+        });
+    }
+
+    private getEngineOptions(): Promise<EngineOptions> {
+        return new Promise<EngineOptions>((resolve, reject) => {
+
+            const engine = vscode.workspace.getConfiguration().get<string>('find.all.references.engine');
+            const engine_options = vscode.workspace.getConfiguration().get<string>('find.all.references.options');
+            if (engine.length<1) {
+                reject(new Error('Error'));
+            }
+            // --column is hardcoded because its mandatory for now
+            const options = '--column '+engine_options;
+
+            console.log("Find All References Is Configured to use :", engine);
+            console.log("Find All References Is Configured with options:", options);
+            // Note to self: if we pass in the additional space after the shell execution, command doesn't get executed correctly when the argument has an extra space.
+            // `rg --column` and not `rg --column `
+            return resolve(new EngineOptions(engine === undefined? 'rg': engine, options.trim()));
+        });
+    }
+
+    private processSearch(document, position, options): Thenable<vscode.Location[]>
+    {
         return new Promise<vscode.Location[]>((resolve, reject) => {
             const searchTerm = document.getText(document.getWordRangeAtPosition(position));
-            const options = this.getEngineOptions();
             // TODO : Introduce the ability to choose different search options ripgrep, silver searcher, git grep, platinum searcher etc
             let rg = child_process.spawnSync(options.getEngine(), [options.getOptions(), searchTerm, vscode.workspace.rootPath]);
             let lines = rg.stdout.toString().split('\n');
             let list = [];
 
             lines.forEach((item) => {
-                console.log(item);
                 let arr = item.split(":");
 
                 if (arr.length>2) {
@@ -36,21 +60,7 @@ export class ReferenceProvider implements vscode.ReferenceProvider {
                 }
             });
 
-        return resolve(list);
-    });
-    }
-
-    private getEngineOptions(): EngineOptions {
-        const engine = vscode.workspace.getConfiguration().get<string>('find.all.references.engine');
-        const engine_options = vscode.workspace.getConfiguration().get<string>('find.all.references.options');
-
-        // --column is hardcoded because its mandatory for now
-        const options = '--column '+engine_options;
-        console.log("Find All References Is Configured to use :", engine);
-        console.log("Find All References Is Configured with options:", options);
-
-        const ops = new EngineOptions(engine === undefined? 'rg': engine, options);
-        console.log('ops', ops);
-        return ops;
+            return resolve(list);
+        });
     }
 }
